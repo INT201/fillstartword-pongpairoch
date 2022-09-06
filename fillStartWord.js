b0cca238f2be157877ae3528f6992147e3be6cef
OAT <88110592+pongpairoch@users.noreply.github.com>
2022-09-07 00:52:53 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,31 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  let result
+  if (word === null || word === undefined){  result = undefined}
+    else if (startWord.length == word.length){
+        if (startWord == word){
+
+            return String(word) 
+        }
+    }
+
+    else {
+        let str = ""
+       
+        for (let index = 0; index < startWord.length; index++) {
+            if (startWord[index] != word[index]){
+            str += startWord[index]
+            }
+        }
+        for (let index = 0; index < word.length; index++) {
+            str += word[index]
+        }    
+
+        return String(str)  
+    }
+  return result
+
 }
+
 module.exports = fillStartWord
